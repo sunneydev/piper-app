556a2082b0327fc579cf49c52b177ba1aa715e01
Sunney-X <sanishubitidze@live.com>
2022-04-24 19:36:43 +0400

    Axios default route change

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
 import { UserProvider } from "./lib/contexts/User.context";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 import Room from "./pages/Room";
 import axios from "axios";
 import React from "react";
-import "./index.css";
-import { createTheme, NextUIProvider } from "@nextui-org/react";
-import Center from "./components/Center";
 import Create from "./pages/Create";
 import Join from "./pages/Join";
 import CenterLayout from "./components/CenterLayout";
+import "./index.css";
 
 const myDarkTheme = createTheme({
   type: "dark",
 });
 
-axios.defaults.baseURL = import.meta.env.PROD
-  ? import.meta.env.API_URL
-  : "http://188.129.204.225:5000";
+axios.defaults.baseURL = "http://188.129.204.225:5000";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
