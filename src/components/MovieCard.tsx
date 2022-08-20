bc6490ae1e777329d8fa9bc46b2fbf7e836e318f
sunney-x <sanishubitidze@live.com>
2022-08-21 00:54:35 +0400

    movie card component

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,33 @@
+import { Button, Card, Col, Image, Row, Text } from "@nextui-org/react";
+import { SearchResult } from "../typings";
+
+const MovieCard: React.FC<SearchResult> = (props) => (
+  <Card css={{ w: "200px", h: "290px" }}>
+    <Card.Body css={{ p: 0 }}>
+      <Card.Image
+        src={props.poster}
+        width="100%"
+        height="100%"
+        objectFit="cover"
+        alt="poster"
+      />
+    </Card.Body>
+    <Card.Footer
+      isBlurred
+      css={{
+        position: "absolute",
+        bgBlur: "#0f111466",
+        bottom: 0,
+        zIndex: 1,
+      }}
+    >
+      <Row align="center">
+        <Text className="truncate max-w-6" size={16} weight="semibold">
+          {props.name} ({props.year})
+        </Text>
+      </Row>
+    </Card.Footer>
+  </Card>
+);
+
+export default MovieCard;
