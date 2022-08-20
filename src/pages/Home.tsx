1a2d7dc35dd4333930e9356c538518289754e1b9
sunney-x <sanishubitidze@live.com>
2022-08-21 00:55:23 +0400

    home rollback

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,15 @@
-import { Button, Modal, Text, Input, Row } from "@nextui-org/react";
-import { useCallback, useState } from "react";
+import { Button, Spacer } from "@nextui-org/react";
+import { Link } from "react-router-dom";
 import Center from "../components/Center";
-import IMovies from "../components/iMovies";
-import Search from "../components/Search";
 
 const Home = () => {
-  const [visible, setVisible] = useState(true);
-
-  const closeHandler = useCallback(() => {
-    setVisible(false);
-  }, []);
-
   return (
     <Center>
-      <Modal
-        closeButton
-        aria-labelledby="modal-title"
-        open={visible}
-        onClose={closeHandler}
-      >
-        <Modal.Header className="flex flex-col">
-          <Input
-            clearable
-            bordered
-            fullWidth
-            color="primary"
-            size="lg"
-            placeholder="Search"
-          />
-        </Modal.Header>
-        <Modal.Body></Modal.Body>
-        <Modal.Footer></Modal.Footer>
-      </Modal>
-      {/* <Link to={"/room/new"}>
+      <Link to={"/room/new"}>
         <Button size={"lg"} color="gradient">
           Create a room
         </Button>
       </Link>
-      <Spacer x={1} /> */}
     </Center>
   );
 };
